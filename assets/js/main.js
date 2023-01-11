039fb4cb41997767a5c8fd70c39e57da5480712c
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-11 13:40:22 +0100

    forms+js_Lev1_1

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function number() {
+    let number = document.getElementById("age");
+    let value = document.getElementById("value");
+    let number2 = number.value*2;
+    value.innerHTML = number2;
+}
